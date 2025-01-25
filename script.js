25d248e7362776c70920b33d4502a9074db4924c
Kunal-Katkar <kunalkatkar.231it033@nitk.edu.in>
2025-01-25 13:50:58 +0530

    ca

--- script.js
+++ script.js
@@ -6,5 +6,5 @@ let hiddenBtn = document.getElementById('openLinkBtn');
 // Add a click event listener to the hidden button
 hiddenBtn.addEventListener('click', () => {
     // Open Google in a new tab
-    window.open('https://drive.google.com/file/d/1-MingRG4D294xIVFAyOy28QCFsObKMjT/view?usp=sharing', '_blank');
+    window.open('https://image-site-one.vercel.app/', '_blank');
 });
